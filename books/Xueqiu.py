9df79af6df1ad2c366c5db82e5fdc6bd72a03d45
Henry Ou <henryouly@gmail.com>
2014-03-06 19:54:44 +0800

    fetch good comments

--- books/Xueqiu.py
+++ books/Xueqiu.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 from base import BaseFeedBook
+import re
+from lib.urlopener import URLOpener
+from bs4 import BeautifulSoup
+import json
 
 def getBook():
     return Xueqiu
@@ -14,11 +18,51 @@ class Xueqiu(BaseFeedBook):
     mastheadfile          = "mh_default.gif"
     coverfile             = "cv_default.jpg"
     oldest_article        = 1
-    
+    fulltext_by_readability = False
+
+    remove_tags = ['meta']
+    remove_attrs = ['xmlns']
+    #keep_only_tags = [dict(name='div', attrs={'class':'statusContent'})]
+
     feeds = [
-            (u'今日话题', 'http://xueqiu.com/hots/topic/rss'),
+            (u'今日话题', 'http://xueqiu.com/hots/topic/rss', True),
             ]
+
+    def processtitle(self, title):
+        self.log.info('Title: %s' % title)
+        return BaseFeedBook.processtitle(self, title)
+
+    def preprocess(self, article):
+        #self.log.info('Preprocess: %s ' % article)
+        return BaseFeedBook.preprocess(self, article)
     
-    def fetcharticle(self, url, decoder):
-        return BaseFeedBook.fetcharticle(self, url, decoder)
-        
+    def postprocess(self, content):
+        pn = re.compile(ur'<a href="(\S*?)">本话题在雪球有.*?条讨论，点击查看。</a>',
+                        re.I)
+        mt = pn.search(content)
+        url = mt.group(1) if mt else None
+        self.log.info(url)
+        if url:
+            opener = URLOpener(url, timeout=self.timeout)
+            result = opener.open(url)
+            if result.status_code == 200 and result.content:
+              if self.feed_encoding:
+                try:
+                  comment = result.content.decode(self.feed_encoding)
+                except UnicodeDecodeError:
+                  return content
+
+        pn = re.compile(r'SNB.data.goodComments\ =\ ({.*?});', re.S | re.I)
+        mt = pn.search(comment)
+        comment_json = mt.group(1) if mt else None
+        j = json.loads(comment_json)
+        soup = BeautifulSoup(content, "lxml")
+        for c in j['comments']:
+            p = soup.new_tag('p')
+            p.string = c['description']
+            self.log.info('desc: %s'%p.string)
+            soup.html.body.append(p)
+
+        content = unicode(soup)
+        self.log.info('Content:%s' % content)
+        return content
