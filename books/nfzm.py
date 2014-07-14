27712856ef73770061583d3bebe317a8e5a57137
mcfloundinho <mcfloundinho@gmail.com>
2014-07-14 13:45:40 +0800

    Use inbuilt login system

--- books/nfzm.py
+++ books/nfzm.py
@@ -1,11 +1,7 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 from bs4 import BeautifulSoup
-from base import BaseFeedBook, string_of_tag
-from urllib import URLopener
-
-EMAIL    = ""    # 南方周末注册邮箱
-PASSWORD = ""    # 南方周末登录密码
+from base import BaseFeedBook, URLOpener, string_of_tag
 
 
 def getBook():
@@ -13,32 +9,33 @@ def getBook():
 
 
 class NFZM(BaseFeedBook):
-    title           = u'南方周末'
-    description     = u'在这里读懂中国 | 每周五更新 | 订阅前请在books/nfzm.py填写账号密码'
-    language        = 'zh-cn'
-    feed_encoding   = "utf-8"
-    page_encoding   = "utf-8"
-    mastheadfile    = "mh_nfzm.gif"
-    coverfile       = "cv_nfzm.jpg"
-    deliver_days    = ['Friday']
+    title               = u'南方周末'
+    description         = u'在这里读懂中国 | 每周五更新 | 需要登录'
+    language            = 'zh-cn'
+    feed_encoding       = "utf-8"
+    page_encoding       = "utf-8"
+    mastheadfile        = "mh_nfzm.gif"
+    coverfile           = "cv_nfzm.jpg"
+    deliver_days        = ['Friday']
+    needs_subscription  = True
 
     def ParseFeedUrls(self):
         login_url = "http://passport.infzm.com/passport/login"
         content_url = "http://www.infzm.com/enews/infzm"
         urls = []
-        opener = URLopener()
-        cookie = opener.open(login_url, data="loginname=%s&password=%s" %
-                             (EMAIL, PASSWORD)).headers.getheader("Set-Cookie")
-        opener.addheader("Cookie", cookie)
+        opener = URLOpener(self.host, timeout=60)
+        login_form = {"loginname":self.account, "password":self.password}
+        login_response = opener.open(login_url, data=login_form)
+        opener.SaveCookies(login_response.header_msg.getheaders('Set-Cookie'))
         result = opener.open(content_url)
-        content = result.read().decode(self.feed_encoding)
+        content = result.content.decode(self.feed_encoding)
         soup = BeautifulSoup(content, "lxml")
         sec_titles = []
         for sec_name in soup.find_all('h2'):
             sec_titles.append(sec_name.get_text())
         for top_news in soup.find_all('dl', {'class': 'topnews'}):
             url = top_news.a['href']
-            feed_content = opener.open(url).read().decode(self.feed_encoding)
+            feed_content = opener.open(url).content.decode(self.feed_encoding)
             feed_soup = BeautifulSoup(feed_content, "lxml")
             urls.append(
                 (sec_titles[0], top_news.a['title'], url, feed_soup.find(id="articleContent")))
@@ -47,7 +44,7 @@ class NFZM(BaseFeedBook):
             for a in sec_content.find_all('a'):
                 url = a['href']
                 feed_content = opener.open(
-                    url).read().decode(self.feed_encoding)
+                    url).content.decode(self.feed_encoding)
                 feed_soup = BeautifulSoup(feed_content, "lxml")
                 urls.append(
                     (sec_titles[sec_count], a['title'], url, feed_soup.find(id="articleContent")))
