93f0324d29508d7d765c5fd09e039e90beea1f09
MuffinC <zj45499@gmail.com>
2014-09-19 15:51:11 +0800

    Add support for Arts & Letters Daily

---
+++ books/aldaily.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+from base import BaseFeedBook
+
+def getBook():
+    return ArtsAndLettersDaily
+
+class ArtsAndLettersDaily(BaseFeedBook):
+    title                 = 'Arts and Letters Daily'
+    description           = 'Delivered Daily. Latest article selections on arts, literatures, and other humanity topics. A must-have for all GRE prepers.'
+    language              = 'en'
+    feed_encoding         = "utf-8"
+    page_encoding         = "utf-8"
+    coverfile             = "cv_aldaily.jpg"
+    mastheadfile          = "mh_aldaily.gif"
+    oldest_article        = 7
+    
+    feeds = [
+            ('http://pipes.yahoo.com/pipes/pipe.run?_id=b268a9d81c3525ef5275eaea242855ad&_render=rss'),
+            ]
