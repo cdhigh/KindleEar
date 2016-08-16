2132f95e3797af62cbddb0817634896a31c26bae
bailonghsu <bailongctui@users.noreply.github.com>
2016-08-16 11:19:52 +0800

    Update makeoeb.py

--- lib/makeoeb.py
+++ lib/makeoeb.py
@@ -89,7 +89,7 @@ def getOpts(output_type='kindle'):
     setattr(opts, "change_justification", "Left")
     setattr(opts, "process_images", True)
     setattr(opts, "mobi_keep_original_images", False)
-    setattr(opts, "graying_image", COLOR_TO_GRAY) #changed
+    setattr(opts, "graying_image", False) #changed
     setattr(opts, "image_png_to_jpg", False) #changed
     setattr(opts, "fix_indents", False)
     setattr(opts, "reduce_image_to", REDUCE_IMAGE_TO or OutputDevice.screen_size)
