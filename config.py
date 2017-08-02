e2ecce97c1b6a1fd14aa42c3beaad0861549f2b5
insert0003 <zheng00032003@gmail.com>
2017-08-02 22:24:32 +0900

    还原TOC的变更
    从兼容性上考虑，留着TOC比较合适

--- config.py
+++ config.py
@@ -31,7 +31,7 @@ ALWAYS_CHAR_DETECT = False
 TRUST_ENCODING_IN_HEADER_OR_META = True
 
 #generate brief description for toc item or not.
-GENERATE_TOC_DESC = False
+GENERATE_TOC_DESC = True
 TOC_DESC_WORD_LIMIT = 500
 
 #-------------------add by rexdf-----------
@@ -39,10 +39,10 @@ TOC_DESC_WORD_LIMIT = 500
 TABLE_OF_CONTENTS = u'Table Of Contents'
 
 #description of toc contains image or not
-GENERATE_TOC_THUMBNAIL = False
+GENERATE_TOC_THUMBNAIL = True
 
 #if generate other html toc or not, just for reading in pc
-GENERATE_HTML_TOC = False
+GENERATE_HTML_TOC = True
 
 #if convert color image to gray or not, good for reducing size of book if you read it in Kindle only
 COLOR_TO_GRAY = True
