a540ffc8ec4522325aaf03a31f93c8f1ed9b6f51
zheng_wenlong <wenlong_zheng@nexty-ele.com>
2018-07-18 15:38:16 +0900

    QQ爬虫修改

--- books/comic/tencentbase.py
+++ books/comic/tencentbase.py
@@ -50,7 +50,7 @@ class TencentBaseBook(BaseComicBook):
             for deliverCount in range(5):
                 newNum = oldNum + deliverCount
                 if newNum < len(chapterList):
-                    if chapterList[newNum]['vip'] == 'lock':
+                    if chapterList[newNum]['vip'] == True:
                         self.log.info("Chapter {} is Vip, waiting for free.".format(newNum))
                         break
                     imgList = self.getImgList(chapterList[newNum])
@@ -93,12 +93,22 @@ class TencentBaseBook(BaseComicBook):
         soup = BeautifulSoup(content, 'lxml')
         # <section class="chapter-list-box list-expanded" data-vip-free="1">
         section = soup.find('section', {'class': 'chapter-list-box list-expanded'})
+        if (section is None):
+            self.log.warn('chapter-list-box is not exist.')
+            return chapterList
+
         # <ul class="chapter-list normal">
         # <ul class="chapter-list reverse">
         reverse_list = section.find('ul', {'class': 'chapter-list reverse'})
+        if (reverse_list is None):
+            self.log.warn('chapter-list is not exist.')
+            return chapterList
+
         for item in reverse_list.find_all('a'):
+            # <a class="chapter-link lock" data-cid="447" data-seq="360" href="/chapter/index/id/531490/cid/447">360</a>
             href = 'http://m.ac.qq.com' + item.get('href')
-            isVip = item.get('class')[1]
+            isVip = "lock" in item.get('class')
+
             chapterList.append({'url':href, 'vip':isVip})
 
         return chapterList
