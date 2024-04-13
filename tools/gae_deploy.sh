a26590fe0cbfa9def21761aff194c38a0a6c7a44
cdhigh <cdhigh@users.noreply.github.com>
2024-04-13 10:01:07 -0300

    Update gae_deploy.sh

--- tools/gae_deploy.sh
+++ tools/gae_deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 gcloud app create   #select a region for app if it's first time
-python ./tools/update_req.py gae
-gcloud beta app deploy --version=1 app.yaml
-gcloud beta app deploy --version=1 cron.yaml
-gcloud beta app deploy --version=1 queue.yaml
+python ./kindleear/tools/update_req.py gae
+gcloud beta app deploy --version=1 ./kindleear/app.yaml
+gcloud beta app deploy --version=1 ./kindleear/cron.yaml
+gcloud beta app deploy --version=1 ./kindleear/queue.yaml
 echo -e "The deployment is completed."
 echo -e "The access address is: https://$GOOGLE_CLOUD_PROJECT.appspot.com"
