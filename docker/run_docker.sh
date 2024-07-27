93de4efcece7026098a0599e4def046f55a9eb3c
cdhigh <cdhigh@users.noreply.github.com>
2024-07-27 16:03:43 -0300

    Update run_docker.sh

--- docker/run_docker.sh
+++ docker/run_docker.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 
-if command -v hexdump > /dev/null 2>&1; then
-  export SECRET_KEY=$(hexdump -n 12 -v -e '/1 "%02x"' /dev/urandom)
-elif command -v od > /dev/null 2>&1; then
-  export SECRET_KEY=$(od -N 12 -A n -t x1 /dev/urandom | tr -d ' \n')
-elif command -v xxd > /dev/null 2>&1; then
-  export SECRET_KEY=$(xxd -l 12 -p /dev/urandom)
-else
-  echo "No command found for SECRET_KEY, using default."
+if [ "$RANDOMIZE_SECRET_KEY" != "no" ]; then
+  if command -v hexdump > /dev/null 2>&1; then
+    export SECRET_KEY=$(hexdump -n 12 -v -e '/1 "%02x"' /dev/urandom)
+  elif command -v od > /dev/null 2>&1; then
+    export SECRET_KEY=$(od -N 12 -A n -t x1 /dev/urandom | tr -d ' \n')
+  elif command -v xxd > /dev/null 2>&1; then
+    export SECRET_KEY=$(xxd -l 12 -p /dev/urandom)
+  else
+    echo "No command found for SECRET_KEY, using default."
+  fi
 fi
 
 if [ -z "$TZ" ]; then
